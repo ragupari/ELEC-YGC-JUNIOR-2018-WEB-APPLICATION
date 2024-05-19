5b1b844afb5a34cc1f5aa21854987421d88e4b1a
pariunimora@gmail.com <pariunimora@gmail.com>
2024-05-19 19:19:37 +0530

    Hello

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ELEC</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta property="og:title" content="anime.js">
+  <meta property="og:url" content="http://animejs.com">
+  <meta property="og:description" content="Javascript Animation Engine">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content="anime.js">
+
+  <meta name="twitter:description" content="Javascript Animation Engine">
+  <meta name="twitter:image" content="http://animejs.com/documentation/assets/img/icons/twitter.png">
+ <link href="incharge/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="documentation/assets/css/anime.css" rel="stylesheet">
+  <link href="documentation/assets/css/site.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <canvas class="fireworks"></canvas>
+  <div class="logo-animation">
+    <div class="letters">
+	
+	
+	
+      <div class="letter letter-a">
+        <svg viewBox="0 0 162 162">
+          <g fill="none" fill-rule="evenodd" stroke="violet">
+            <path class="fill in" stroke-width="30" d="M81 101h60V81c-1-33.14-26.86-60-60-60a60 60 0 1 0 0 120"/>
+            <path class="fill out" stroke-width="30" d="M81 101h60V81c-1-33.14-26.86-60-60-60a60 60 0 1 0 0 120"/>
+            <path class="line out" stroke-width="2" d="M81 101h60V81c-1-33.14-26.86-60-60-60a60 60 0 1 0 0 120"/>
+          </g>
+        </svg>
+      </div>
+      <div class="letter letter-n">
+        <svg viewBox="0 0 162 162">
+         
+        </svg>
+      </div>
+      
+      <div class="letter letter-m-1">
+        <svg viewBox="0 0 162 162">
+          <g fill="none" fill-rule="evenodd" stroke="#18FF92">
+            <path class="fill in" stroke-width="40"  d="M21 161V1"/>
+            <path class="fill out" stroke-width="40" d="M21 1v160"/>
+			<path class="line out" stroke-width="10" d="M21 1v160"/>
+             </g>
+        </svg>
+      </div>
+     
+      <div class="letter letter-e">
+        <svg viewBox="0 0 162 162">
+          <g fill="none" fill-rule="evenodd" stroke="#5E89FB">
+            <path class="fill in" stroke-width="30" d="M81 101h60V81c-1-33.14-26.86-60-60-60a60 60 0 1 0 0 120"/>
+            <path class="fill out" stroke-width="30" d="M81 101h60V81c-1-33.14-26.86-60-60-60a60 60 0 1 0 0 120"/>
+            <path class="line out" stroke-width="10" d="M81 101h60V81c-1-33.14-26.86-60-60-60a60 60 0 1 0 0 120"/>
+          </g>
+        </svg>
+      </div>  
+       <div class="letter letter-n">
+        <svg viewBox="0 0 162 162">
+         
+        </svg>
+      </div>
+    <div class="letter letter-m-1">
+        <svg viewBox="0 0 162 162">
+          <g fill="none" fill-rule="evenodd" stroke="pink">
+            <path class="fill in" stroke-width="30" d="M101 141H81a60 60 0 1 1 0-120c33.14 0 59 26.86 62 "/>
+            <path class="fill out" stroke-width="30"d="M101 141H81a60 60 0 1 1 0-120c33.14 0 59 26.86 62"/>
+            <path class="line out" stroke-width="2" d="M101 141H81a60 60 0 1 1 0-120c33.14 0 59 26.86 62"/>
+          </g>
+        </svg>
+      </div>
+        <div class="dot dot-e">
+        <svg viewBox="0 0 42 42">
+          <g fill="none" fill-rule="evenodd" >
+            <rect width="20" height="20" x="1" y="1" fill="orange" rx="20"/>
+          </g>
+        </svg>
+      </div>
+    </div>
+  </div>
+  <div class="info">
+    <p class="description"><font size="350">
+    	  <i class="fa fa-fw fa-quote-left"></i>    MAKE YOUR VOICE HEARD... <i class="fa fa-fw fa-quote-right"></i> 
+    </p></font>
+ 
+    <div class="links">
+     
+      <a class="button blue" href="incharge/index.php" >
+        <svg viewBox="0 0 180 100">
+          <path d="M10,10 C10,10 50,9.98999977 90,9.98999977 C130,9.98999977 170,10 170,10 C170,10 170.009995,20 170.009995,30 C170.009995,40 170,50 170,50 C170,50 130,50.0099983 90,50.0099983 C50,50.0099983 10,50 10,50 C10,50 9.98999977,40 9.98999977,30 C9.98999977,20 10,10 10,10 Z"></path>
+        </svg>
+        <span><i class="fa fa-fw fa-arrow-right"></i> <strong> GET STARTED</strong></span>
+      </a>
+     
+    </div>
+    <p class="credits">
+      WEB PROJECT <span class="version">0.0001</span> FOR YGC SEASON 07 @<span class="date">1900 </p>
+  </div>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-79927007-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+  <script src="anime.min.js"></script>
+  <script src="documentation/assets/js/fireworks.js"></script>
+  <script src="documentation/assets/js/site.js"></script>
+</body>
+</html>
